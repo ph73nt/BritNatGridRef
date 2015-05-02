6301443410b3ec641daab12cee8a9ee6f3af3358
neil <njt@fishlegs.co.uk>
2015-05-02 23:12:28 +0100

    Now loads from CSV file

--- BritNatGridRef.py
+++ BritNatGridRef.py
@@ -80,6 +80,12 @@ class BritNatGridRef:
         # Prep error messages
         self.msgBadGrid = "Sorry, I could not process the request; incorrect grid reference format"
         self.msgNoLayer = "Sorry, an error occurred - check there is a writable active layer"
+        
+        # Make hashtable of eastings and northings by grid reference
+        self.east = {'HP':'4', 'HT':'3', 'HU':'4', 'HW':'1', 'HX':'2', 'HY':'3', 'HZ':'4', 'NA':'0', 'NB':'1', 'NC':'2', 'ND':'3', 'NF':'0', 'NG':'1', 'NH':'2', 'NJ':'3', 'NK':'4', 'NL':'0', 'NM':'1', 'NN':'2', 'NO':'3', 'NR':'1', 'NS':'2', 'NT':'3', 'NU':'4', 'NW':'1', 'NX':'2', 'NY':'3', 'NZ':'4', 'OV':'5', 'SC':'2', 'SD':'3', 'SE':'4', 'SH':'2', 'SJ':'3', 'SK':'4', 'SM':'1', 'SN':'2', 'SO':'3', 'SP':'4', 'SR':'1', 'SS':'2', 'ST':'3', 'SU':'4', 'SV':'0', 'SW':'1', 'SX':'2', 'SY':'3', 'SZ':'4', 'TA':'5', 'TF':'5', 'TG':'6', 'TL':'5', 'TM':'6', 'TQ':'5', 'TR':'6', 'TV':'5'}
+        self.north = {'HP':'12', 'HT':'11', 'HU':'11', 'HW':'10', 'HX':'10', 'HY':'10', 'HZ':'10', 'NA':'9', 'NB':'9', 'NC':'9', 'ND':'9', 'NF':'8', 'NG':'8', 'NH':'8', 'NJ':'8', 'NK':'8', 'NL':'7', 'NM':'7', 'NN':'7', 'NO':'7', 'NR':'6', 'NS':'6', 'NT':'6', 'NU':'6', 'NW':'5', 'NX':'5', 'NY':'5', 'NZ':'5', 'OV':'5', 'SC':'4', 'SD':'4', 'SE':'4', 'SH':'3', 'SJ':'3', 'SK':'3', 'SM':'2', 'SN':'2', 'SO':'2', 'SP':'2', 'SR':'1', 'SS':'1', 'ST':'1', 'SU':'1', 'SV':'0', 'SW':'0', 'SX':'0', 'SY':'0', 'SZ':'0', 'TA':'4', 'TF':'3', 'TG':'3', 'TL':'2', 'TM':'2', 'TQ':'1', 'TR':'1', 'TV':'0'}  
+
+
 
     def fileButtonClicked(self):
         #Show file picker dialogue
@@ -208,15 +214,15 @@ class BritNatGridRef:
         
         qgis.utils.iface.messageBar().pushMessage("Error", self.msgBadGrid, level=QgsMessageBar.CRITICAL)
 
-    def addPoint(self, east, north, ref):
+    def addPoint(self, ref, attrs):
         
         refLen = len(ref)
         
         if refLen > 3:
             code = ref[0:2].upper()
             print code
-            e = east[code]
-            n = north[code]
+            e = self.east[code]
+            n = self.north[code]
             print "East: ", e, " North: ", n
             # Should now have the northings and eastings values... now need to add some more precision
             metres = ref[2:]
@@ -243,19 +249,22 @@ class BritNatGridRef:
                 n += nMetres
                 print e
                 print n
+                
                 # Great... now have out coordinates in QGIS acceptable format... add a new point
                 # Get active layer
                 layer = qgis.utils.iface.activeLayer()
                 caps = layer.dataProvider().capabilities()
                 if caps:
+                    
                     # Declare a feature
                     feat = QgsFeature()
+                    
                     # Add attributes
-                    attrs = self.dlg.textAttributes.text()
                     if len(attrs) > 0: # Just use comma delimited attList
                         attList = attrs.split(",")
                         print attList
                         feat.setAttributes(attList)
+                    
                     # Add the points
                     feat.setGeometry(QgsGeometry.fromPoint(QgsPoint(int(e), int(n))))
                     res, outFeats = layer.dataProvider().addFeatures([feat])
@@ -263,8 +272,10 @@ class BritNatGridRef:
                         qgis.utils.iface.messageBar().pushMessage("Error", "An error occurred adding points", level=QgsMessageBar.CRITICAL)
                         self.stayOpen = False
                         return 1
+                    
                     # Must update the UI for user to enjoy the new points
                     layer.triggerRepaint()
+                
                 else:
                     self.badGridError()    
         else:
@@ -276,11 +287,6 @@ class BritNatGridRef:
 
     def run(self):
       
-        # Make hashtable of eastings and northings by grid reference
-        east = {'HP':'4', 'HT':'3', 'HU':'4', 'HW':'1', 'HX':'2', 'HY':'3', 'HZ':'4', 'NA':'0', 'NB':'1', 'NC':'2', 'ND':'3', 'NF':'0', 'NG':'1', 'NH':'2', 'NJ':'3', 'NK':'4', 'NL':'0', 'NM':'1', 'NN':'2', 'NO':'3', 'NR':'1', 'NS':'2', 'NT':'3', 'NU':'4', 'NW':'1', 'NX':'2', 'NY':'3', 'NZ':'4', 'OV':'5', 'SC':'2', 'SD':'3', 'SE':'4', 'SH':'2', 'SJ':'3', 'SK':'4', 'SM':'1', 'SN':'2', 'SO':'3', 'SP':'4', 'SR':'1', 'SS':'2', 'ST':'3', 'SU':'4', 'SV':'0', 'SW':'1', 'SX':'2', 'SY':'3', 'SZ':'4', 'TA':'5', 'TF':'5', 'TG':'6', 'TL':'5', 'TM':'6', 'TQ':'5', 'TR':'6', 'TV':'5'}
-        north = {'HP':'12', 'HT':'11', 'HU':'11', 'HW':'10', 'HX':'10', 'HY':'10', 'HZ':'10', 'NA':'9', 'NB':'9', 'NC':'9', 'ND':'9', 'NF':'8', 'NG':'8', 'NH':'8', 'NJ':'8', 'NK':'8', 'NL':'7', 'NM':'7', 'NN':'7', 'NO':'7', 'NR':'6', 'NS':'6', 'NT':'6', 'NU':'6', 'NW':'5', 'NX':'5', 'NY':'5', 'NZ':'5', 'OV':'5', 'SC':'4', 'SD':'4', 'SE':'4', 'SH':'3', 'SJ':'3', 'SK':'3', 'SM':'2', 'SN':'2', 'SO':'2', 'SP':'2', 'SR':'1', 'SS':'1', 'ST':'1', 'SU':'1', 'SV':'0', 'SW':'0', 'SX':'0', 'SY':'0', 'SZ':'0', 'TA':'4', 'TF':'3', 'TG':'3', 'TL':'2', 'TM':'2', 'TQ':'1', 'TR':'1', 'TV':'0'}  
-
-
         # Check hashtable actually returns something and no other funnies are caught
         try:        
         
@@ -304,13 +310,15 @@ class BritNatGridRef:
                         # Check if dialogue should stay open
                         self.stayOpen = self.dlg.addMore.isChecked()
                                         
-                        # Check for a match
+                        # Get grid ref and attributes
                         ref = self.dlg.textGridRef.text()
-                        
-                        addOkay = self.addPoint(east, north, ref)
+                        attrs = self.dlg.textAttributes.text()
+                    
+                        addOkay = self.addPoint(ref, attrs)
                         
                         if addOkay != 0:
                             self.stayOpen = False
+                            self.badGridError()
                     
                     # Add CSV file 
                     elif index == 1:
@@ -325,7 +333,39 @@ class BritNatGridRef:
                         with open(csvFileName, 'rb') as csvfile:
                             r = csv.reader(csvfile, delimiter=delim)
                             for row in r:
+                                
                                 print ', '.join(row)
+                                self.lineCount += 1
+                                
+                                i = 0
+                                gridRef = ""
+                                attribs = ""
+    
+                                for cell in row:
+                                    
+                                    if i == 0:
+                                        gridRef = cell
+                                    
+                                    else:
+                                        attribs += cell
+                                        attribs += ","
+                                        
+                                    i += 1
+                                        
+                                print gridRef
+                                print attribs
+                                # Strip final comma
+                                attribs = attribs.rstrip(",")
+                                print attribs
+                                    
+                                # Now add to grid
+                                addOK = self.addPoint(gridRef, attribs)
+                                
+                                # Stop and report if an error occurs
+                                if addOK != 0:
+                                    self.stayOpen = False
+                                    self.badGridError()
+                                    break
                                 
                         self.stayOpen = False
                     
